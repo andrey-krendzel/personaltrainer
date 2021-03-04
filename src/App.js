17541aafbd9cff27c0909f4517871934c39cf06a
unknown <andreykrendzel@hotmail.com>
2021-03-04 01:26:42 -0800

    Updated whatever fuck this sbhit

--- src/App.js
+++ src/App.js
@@ -11,14 +11,26 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import TableSortLabel from '@material-ui/core/TableSortLabel';
 import Toolbar from '@material-ui/core/Toolbar';
-import { DataGrid } from '@material-ui/data-grid';
 
+import { AgGridColumn, AgGridReact } from 'ag-grid-react';
+
+import 'ag-grid-enterprise';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 function App(props) {
   const [update, setUpdate] = useState();
   const [customers, setCustomers] = useState([]);
   const [sortedField, setSortedField] = React.useState();
   const [direction, setDirection] = React.useState();
+
+  const [gridApi, setGridApi] = useState(null);
+  const [gridColumnApi, setGridColumnApi] = useState(null);
+
+  const onGridReady = params => {
+    setGridApi(params.api);
+    setGridColumnApi(params.columnApi);
+}
 
 
     //Errors for fetch
@@ -77,215 +89,22 @@ function App(props) {
   return (
     <div className="App">
 
-
-        <TableContainer component={Paper}>
-      <Table aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>ID </TableCell>
+      <div className="ag-theme-alpine" style={{ height: 400, width: 1000 }}>
+            <AgGridReact
+                onGridReady={onGridReady}
+                rowData={customers}>
+             <AgGridColumn field="firstname" sortable={ true } filter={ true }></AgGridColumn>
+             <AgGridColumn field="lastname" sortable={ true } filter={ true }></AgGridColumn>
+             <AgGridColumn field="streetaddress" sortable={ true } filter={ true }></AgGridColumn>
+             <AgGridColumn field="postcode" sortable={ true } filter={ true }></AgGridColumn> 
+             <AgGridColumn field="phone" sortable={ true } filter={ true }></AgGridColumn> 
+             <AgGridColumn field="email" sortable={ true } filter={ true }></AgGridColumn> 
             
-            <TableCell align="right">First name</TableCell>
-            <TableCell align="right">Last name</TableCell>
-            <TableCell align="right">Street address</TableCell>
-            <TableCell align="right">Post code</TableCell>
-            <TableCell align="right">City</TableCell>
-            <TableCell align="right">Email</TableCell>
-            <TableCell align="right">Phone</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {customers.map((customer, index) => (
-            <TableRow key={index}>
-              <TableCell component="th" scope="row">
-              {customer.links[0].href.split("/")[5]}
-              </TableCell>
-              <TableCell align="right">{customer.firstname}</TableCell>
-              <TableCell align="right">{customer.lastname}</TableCell>
-              <TableCell align="right">{customer.streetaddress}</TableCell>
-              <TableCell align="right">{customer.postcode}</TableCell>
-              <TableCell align="right">{customer.city}</TableCell>
-              <TableCell align="right">{customer.email}</TableCell>
-              <TableCell align="right">{customer.phone}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-     <table>
-          <thead>
-              <tr>
-                 <th> ID:</th>
-                  <th>First name: &nbsp;
-              <button
-                type="button"
-                onClick={() => {
-                  setSortedField("firstname");
-                  setDirection("asc");
-                }}
-              >
-                Asc
-              </button>
+            </AgGridReact>
+        </div>
 
-              <button
-                type="button"
-                onClick={() => {
-                  setSortedField("firstname");
-                  setDirection("desc");
-                }}
-              >
-                Desc
-              </button>
-              </th>
-                  <th>Last name: &nbsp;
-              <button
-                type="button"
-                onClick={() => {
-                  setSortedField("lastname");
-                  setDirection("asc");
-                }}
-              >
-                Asc
-              </button>
-
-              <button
-                type="button"
-                onClick={() => {
-                  setSortedField("lastname");
-                  setDirection("desc");
-                }}
-              >
-                Desc
-              </button>
-
-                  </th>
-
-
-                  <th>Street address:  &nbsp;
-              <button
-                type="button"
-                onClick={() => {
-                  setSortedField("streetaddress");
-                  setDirection("asc");
-                }}
-              >
-                Asc
-              </button>
-
-              <button
-                type="button"
-                onClick={() => {
-                  setSortedField("streetaddress");
-                  setDirection("desc");
-                }}
-              >
-                Desc
-              </button></th>
-                  <th>Postcode: &nbsp;
-
-                  <button
-                type="button"
-                onClick={() => {
-                  setSortedField("postcode");
-                  setDirection("asc");
-                }}
-              >
-                Asc
-              </button>
-
-              <button
-                type="button"
-                onClick={() => {
-                  setSortedField("postcode");
-                  setDirection("desc");
-                }}
-              >
-                Desc
-              </button>
-                    
-                  </th>
-                  <th>City:  &nbsp;
-                
-                 <button
-                type="button"
-                onClick={() => {
-                  setSortedField("city");
-                  setDirection("asc");
-                }}
-              >
-                Asc
-              </button>
-
-              <button
-                type="button"
-                onClick={() => {
-                  setSortedField("city");
-                  setDirection("desc");
-                }}
-              >
-                Desc
-              </button></th>
-                  <th>Email: &nbsp;
-                
-                <button
-               type="button"
-               onClick={() => {
-                 setSortedField("email");
-                 setDirection("asc");
-               }}
-             >
-               Asc
-             </button>
-
-             <button
-               type="button"
-               onClick={() => {
-                 setSortedField("email");
-                 setDirection("desc");
-               }}
-             >
-               Desc
-             </button></th>
-                  <th>Phone: &nbsp;
-                
-                <button
-               type="button"
-               onClick={() => {
-                 setSortedField("phone");
-                 setDirection("asc");
-               }}
-             >
-               Asc
-             </button>
-
-             <button
-               type="button"
-               onClick={() => {
-                 setSortedField("phone");
-                 setDirection("desc");
-               }}
-             >
-               Desc
-             </button></th>
-                 <th></th>
-                 <th></th>
-              </tr>
-              </thead>
-          {customers.map((customer, index) =>  
-        <tbody>
-        <tr key={index}>
-          <td>{customer.links[0].href.split("/")[5]}</td>
-          <td>{customer.firstname}</td>
-          <td>{customer.lastname}</td>
-          <td>{customer.streetaddress}</td>
-          <td>{customer.postcode}</td>
-          <td>{customer.city}</td>
-          <td>{customer.email}</td>
-          <td>{customer.phone}</td>
-          <td><button>Delete</button></td>
-          <td><button>Edit</button></td>
-          </tr>
-          </tbody>)}
-          </table> 
+        
+    
     </div>
   );
 }

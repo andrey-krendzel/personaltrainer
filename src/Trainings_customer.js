83185be37ad5aae76919950bbeac1ff33ac944d2
user <andreykrendzel@hotmail.com>
2021-03-24 13:15:15 +0200

    Trainings_customer add training, tabs

--- src/Trainings_customer.js
+++ src/Trainings_customer.js
@@ -3,6 +3,51 @@ import './App.css';
 import { useState } from "react";
 import React from "react";
 import { Button, Table, Card, Accordion } from 'react-bootstrap';
+import TextField from "@material-ui/core/TextField";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+
+function AddTrainings(props) {
+  return (
+    <div className="addTrainings">
+      <br />
+      <h2> Add new training </h2>
+      <TextField
+        name="customerId"
+        label="Customer ID"
+        value={props.customerId}
+      />
+      <br />
+      <TextField
+        name="duration"
+        label="Duration of training"
+        onChange={props.addTrainingInputChanged}
+        value={props.newTraining.duration}
+      />
+      <br />
+      <TextField
+        name="activity"
+        label="Activity"
+        onChange={props.addTrainingInputChanged}
+        value={props.newTraining.activity}
+      />
+       <br />
+      <TextField
+        name="date"
+        label="Date"
+        onChange={props.addTrainingInputChanged}
+        value={props.newTraining.date}
+      />
+      <br />
+      <br />
+      <Button onClick={props.addTraining} variant="primary" >
+        Add training
+      </Button>
+      <hr></hr>
+    </div>
+  );
+}
 
 
 function Trainings(props) {
@@ -10,6 +55,12 @@ function Trainings(props) {
   const [trainings, setTrainings] = useState([]);
   const [sortedField, setSortedField] = React.useState();
   const [direction, setDirection] = React.useState();
+  const [tabValue, setTabValue] = useState("one");
+  const [newTraining, setNewTraining] = useState({
+    duration: "",
+    activity: "",
+    date: "",
+  });
 
     //Errors for fetch
     function handleErrors(response) {
@@ -31,6 +82,42 @@ function Trainings(props) {
       })
       .catch((error) => console.log(error));
   }, [update]);
+
+// Add new training: handle input changed
+const addTrainingInputChanged = (event) => {
+  setNewTraining({ ...newTraining, [event.target.name]: event.target.value });
+};
+
+// ADD TRAINING
+const addTraining = (event) => {
+  event.preventDefault();
+
+  
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      customer: "https://customerrest.herokuapp.com/api/customers/" + props.location.customerId,
+      duration: newTraining.duration,
+      activity: newTraining.activity,
+      date: newTraining.date
+    }),
+  };
+
+  fetch("https://customerrest.herokuapp.com/api/trainings", requestOptions)
+    .then(handleErrors)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
+
+    setUpdate(Math.random())
+  setNewTraining({
+    duration: "",
+    activity: "",
+    date: ""
+  });
+};
+
 
     //Sort magic
   //Dont execute the function if sortedField isn't modified
@@ -83,10 +170,29 @@ function Trainings(props) {
   }
   };
 
+   // Tabs
+   const handleTabChange = (event, tabValue) => {
+    setTabValue(tabValue);
+  };
+
+
+
 
   return (
     <div className="Trainings">
       <h2>Customer id: <i>{props.location.customerId}</i></h2>
+      <AppBar position="static">
+          <Tabs value={tabValue} onChange={handleTabChange}>
+            <Tab value="one" label="View trainings" />
+            <Tab value="two" label="Add trainings" />
+            <Tab value="three" label="Export trainings" />
+          </Tabs>
+        </AppBar>
+        {tabValue === "one" && (
+          <p>Filter placeolder</p> 
+        )}
+         {tabValue === "two" && ( <AddTrainings newTraining={newTraining} addTrainingInputChanged={addTrainingInputChanged} addTraining={addTraining} />)}
+         {tabValue === "three" && ( <p>Export customer placeolder</p> )}
      <Table striped bordered hover>
           <thead>
               <tr>
